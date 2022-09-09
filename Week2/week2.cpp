ae2e66f38326568e0a17ca4718412a1af26ffd6f
Trung N Vong <trungnvong956@g.ucla.edu>
2022-09-09 04:54:16 -0700

    updated week2 recursion examples

--- Week2/week2.cpp
+++ Week2/week2.cpp
@@ -77,6 +77,28 @@ int fib_memo(int num)
     return fib_memo(num - 1) + fib_memo(num - 2);
 }
 
+// Compute fib memo with push back with recursive function
+int fib_memo_2(int num)
+{
+    static vector<int> memo(0, 1);
+    if (memo.size() >= (num + 1))
+    {
+        return memo[num];
+    }
+    int f_n = fib_memo_2(num - 2) + fib_memo_2(num - 1);
+    memo.push_back(f_n);
+    return f_n;
+}
+
+// Compute fib with 3 arguments
+int fib_helper(int num, int a, int b)
+{
+    if (num == 0)
+    {
+        return a;
+    }
+    return fib_helper(num - 1, b, a + b);
+}
 // Print hello with n times
 void print_hello(int num)
 {
@@ -88,6 +110,45 @@ void print_hello(int num)
     print_hello(num - 1);
 }
 
+// 20 12 | 8 12 | 8 4 | 4 4
+// Example: 20 % 12 = 8
+// 12 % 8 = 4
+// 8 % 4 = 0
+// Always modulus with the largest number and return the smallest num.
+
+// Num1 > num2
+int gcd_modulus(int num1, int num2)
+{
+    cout << "num1: " << num1 << endl;
+    cout << "num2: " << num2 << endl;
+    if (num2 == 0)
+    {
+        return num1;
+    }
+    return gcd_modulus(num2, num1 % num2);
+}
+
+// No needed
+int gcd_modulus_2(int num1, int num2)
+{
+    if (num2 == 0)
+    {
+        return num1;
+    }
+    if (num1 == 0)
+    {
+        return num2;
+    }
+    if (num1 == num2)
+    {
+        return num1;
+    }
+    if (num1 < num2)
+    {
+        return gcd_modulus_2(num2, num2 % num1);
+    }
+    return gcd_modulus_2(num2, num1 % num2);
+}
 // Templates class
 template <class T>
 T maxNum(T num1, T num2)
@@ -96,15 +157,16 @@ T maxNum(T num1, T num2)
 };
 int main()
 {
-    // for (int index = 0; index < 5; index++)
+    // for (int index = 0; index < 10; index++)
     // {
-    //     // cout << factorial(index) << endl;
+    //     // cout << fib_memo_2(index) << endl;
     //     // cout << "-------" << endl;
-    //     cout << fib_memo(index) << endl;
+    //     cout << fib_helper(index, 0, 1) << endl;
     // }
     // print_hello(10);
     // int max = maxNum(10, 20);
     // cout << "Max Number is: " << max << endl;
-
+    cout << "Method 1: " << gcd_modulus(20, 12) << endl;
+    // cout << "Method 2: " << gcd_modulus_2(50, 50) << endl;
     return 0;
 }
