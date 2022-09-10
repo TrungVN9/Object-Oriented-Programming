dfc9b10a4763759097e228e914ec1278d4a5dae9
Trung N Vong <trungnvong956@g.ucla.edu>
2022-09-09 21:09:50 -0700

    taylor series recursion

--- Recursion/recursion.cpp
+++ Recursion/recursion.cpp
@@ -66,6 +66,42 @@ int nested_func(int n)
 int sum(int a){
     return (a == 0) ? 0: a + sum(a - 1);
 }
+
+int func_e(int x, int n){
+    static int p = 1, f = 1;
+    int r; 
+    if (n==0){return 1;}
+    r = func_e(x, n-1); 
+    p = p * x;
+    f = f* n;
+    return r + p/f;
+}
+
+int taylor_series(int x, int num){
+    //e^x = 1 + (x/1) + (x^2/2!) + (x^3/3!) + (x^4/4!) + ... + n
+    //using common factor: 1 + (x/1)*(1 + x/2*(1+x/3))
+    int s = 1;
+    for (num; num > 0; num--){
+        s = 1 + (x/n)*s;
+    }
+    return s;
+}
+//Tower of Hanoi
+void tower_Hanoi(int num_disk, int A, int B, int C){
+    if (num_disk > 0){
+        tower_Hanoi(num_disk - 1, A, C, B);
+        cout << "from " << A << " to " << C <<endl;
+        tower_Hanoi(num_disk - 1, B, A, C);
+    }
+}
+int taylor_series_rec(int x, int num){
+    static int s = 1;
+    if (num == 0){
+        return s;
+    }
+    s = 1 + (x/num) * s;
+    return taylor_series_rec(x, n-1);
+}
 int main()
 {
     int value = 3;
