c4d880ea7d1fcc872b95fcb861b3d558d0936717
TrungVN9 <trungnvong956@g.ucla.edu>
2022-10-04 11:34:52 -0700

    added function practices

--- SI_Materials/6th_week_1_session.cpp
+++ SI_Materials/6th_week_1_session.cpp
@@ -10,8 +10,27 @@ bool is_even(int num)
     }
     return false;
 }
-int square_nums(int num){
+int square_nums(int num)
+{
     return num * num;
+}
+bool is_divisible_by_5(int num)
+{
+    if (num % 5 == 0)
+        return true;
+    return false;
+}
+bool is_divisible_by_7(int num)
+{
+    if (num % 7 == 0)
+        return true;
+    return false;
+}
+bool is_divisible_by_11(int num)
+{
+    if (num % 11 == 0)
+        return true;
+    return false;
 }
 int main()
 {
@@ -19,6 +38,9 @@ int main()
     // A function that can check even and odd number
     int sum_even = 0;
     int sum_odd = 0;
+    int mul = 1;
+    int sum_seven = 0;
+    int sum_eleven = 0;
     for (int num = 1; num <= 10; num++)
     {
         if (is_even(num))
@@ -35,4 +57,24 @@ int main()
     }
     cout << "Sum even number: " << sum_even << endl;
     cout << "Sum odd number: " << sum_odd << endl;
+    cout << "==========================================" << endl;
+    for (int num1 = 20; num1 <= 40; num1++)
+    {
+        if (is_divisible_by_5(num1))
+        {
+            mul *= num1;
+        }
+        else if (is_divisible_by_7(num1))
+        {
+            sum_seven += num1;
+        }
+        else if (is_divisible_by_11(num1))
+        {
+            sum_eleven += num1;
+        }
+    }
+    cout << "Multiplication of numbers divisble by 5: " << mul << endl;
+    cout << "Sum of numbers divisble by 7: " << sum_seven << endl;
+    cout << "Sum of numbers divisble by 11: " << sum_eleven << endl;
+    return 0;
 }
