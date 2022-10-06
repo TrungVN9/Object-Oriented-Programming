9d51f8f3c8f51e974eda6a606ce9429c2bfd77e2
TrungVN9 <trungnvong956@g.ucla.edu>
2022-10-06 12:03:49 -0700

    Updated header file

---
+++ SI_Materials/temp_header.h
@@ -0,0 +1,5 @@
+template <class T>
+T multiply_two_nums(T num1, T num2)
+{
+    return num1 * num2;
+}
---
+++ SI_Materials/template_func.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "temp_header.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Multiply two numbers: " << multiply_two_nums(2, 3) << endl;
+    return 0;
+}
