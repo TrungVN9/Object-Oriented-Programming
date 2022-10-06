b6d3900b677879ba46cd81ff5ca2a61601770b66
TrungVN9 <trungnvong956@g.ucla.edu>
2022-10-06 12:11:28 -0700

    Max and min of two numbers

--- SI_Materials/temp_header.h
+++ SI_Materials/temp_header.h
@@ -3,3 +3,24 @@ T multiply_two_nums(T num1, T num2)
 {
     return num1 * num2;
 }
+
+template <class N>
+N max_num(N num1, N num2)
+{
+    N temp_max = num1;
+    if (num2 > temp_max)
+    {
+        temp_max = num2;
+    }
+    return temp_max;
+}
+template <class M>
+M min_num(M num1, M num2)
+{
+    M temp_min = num1;
+    if (num1 < temp_min)
+    {
+        temp_min = num1;
+    }
+    return temp_min;
+}
--- SI_Materials/template_func.cpp
+++ SI_Materials/template_func.cpp
@@ -7,5 +7,8 @@ using namespace std;
 int main()
 {
     cout << "Multiply two numbers: " << multiply_two_nums(2, 3) << endl;
+    cout << "Max of 2 nums: " << max_num(10.0, 10.1) << endl;
+    cout << "Min of 2 nums: " << min_num(5, 10) << endl;
+
     return 0;
 }
